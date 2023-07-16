6e386adb3b40dd2eb91452afa65bbe709bb2ab05
Akidevge <zurajanai737@gmail.com>
2023-07-16 09:22:54 +0530

    Added Challenge for layout

--- lib/main.dart
+++ lib/main.dart
@@ -14,35 +14,51 @@ class Myapp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.lightBlueAccent,
         body: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-                height: 100,
-                width: 100,
-                color: Colors.white,
-                child: const Text(
-                  'Container 1',
-                  textAlign: TextAlign.center,
-                )),
-            Container(
-                height: 100,
-                width: 100,
-                color: Colors.orange,
-                child: const Text(
-                  'Container 2',
-                  textAlign: TextAlign.center,
-                )),
-            Container(
-                height: 100,
-                width: 100,
-                color: Colors.red,
-                child: const Text(
-                  'Container 3',
-                  textAlign: TextAlign.center,
-                )),
-          ],
-        )),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                  width: 100.0,
+                  color: Colors.red,
+                  child: const Text(
+                    'Row 1',
+                    textAlign: TextAlign.center,
+                  )),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                      child: const Text(
+                        'Column 1',
+                        textAlign: TextAlign.center,
+                      )),
+                  Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.green,
+                      child: const Text(
+                        'Column 2',
+                        textAlign: TextAlign.center,
+                      )),
+                ],
+              ),
+              // const SizedBox(
+              //   width: 192.7,
+              // ),
+              Container(
+                  width: 100.0,
+                  color: Colors.teal,
+                  child: const Text(
+                    'Row 2',
+                    textAlign: TextAlign.center,
+                  )),
+            ],
+          ),
+        ),
       ),
     );
   }
