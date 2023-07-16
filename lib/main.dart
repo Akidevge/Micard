8d4728ffdb6b6b067bfab224feaa2737a397b996
Akidevge <zurajanai737@gmail.com>
2023-07-16 12:41:31 +0530

    Added font and container for icons

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,75 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 100.0,
-                backgroundImage: AssetImage('Aki.jpg'),
-              )
-            ],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: AssetImage('images/Aki.jpg'),
+                ),
+                const Text(
+                  'Akidagoat',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'SourceCodePro',
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 25.0),
+                  padding: const EdgeInsets.all(10),
+                  child: const Row(
+                    children: [
+                      Icon(Icons.phone),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text('+91 88 11 22 44'),
+                      // Icon(Icons.email),
+                      // SizedBox(
+                      //   width: 20.0,
+                      // ),
+                      // Text('abc@gmail.com'),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 25.0),
+                  padding: const EdgeInsets.all(10),
+                  child: const Row(
+                    children: [
+                      Icon(Icons.email),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text('abc@gmail.com'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
